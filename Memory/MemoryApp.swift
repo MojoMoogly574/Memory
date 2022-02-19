12404e389c3e4f319218e1e169ef0faad5cb1974
Mojo <mojo@nuralab.com>
2022-02-19 13:03:27 -0600

    Initial Commit

---
+++ Memory/MemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryApp.swift
+//  Memory
+//
+//  Created by Joseph DeWeese on 2/19/22.
+//
+
+import SwiftUI
+
+@main
+struct MemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
