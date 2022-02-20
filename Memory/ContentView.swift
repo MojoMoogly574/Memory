6dd9f1cc8ebbd0b40f2487322382c6fe691e1f4b
Mojo <mojo@nuralab.com>
2022-02-20 10:11:27 -0600

    cleanup - add and remove button

--- Memory/ContentView.swift
+++ Memory/ContentView.swift
@@ -8,19 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        HStack {
-            CardView()
-            CardView()
-            CardView()
-            CardView()
-        }.padding()
-            .foregroundColor(.red)
-    }
-    }
-struct CardView: View {
+    //Properties...
+    @State var emojis = ["〽️","👊🏽","😂","🤷","💃🏾","🧏🏽‍♀️","👑","💍"]
+    @State var emojiCount = 4
     
-    var isFaceUp: Bool = false
+    
+    var body: some View {
+        VStack {
+            HStack {
+                ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                    CardView(content: emoji)
+                }
+            }
+            //HStack
+            HStack {
+                remove
+                Spacer()
+                add
+            }
+            .padding()
+            .font(.largeTitle)
+            Spacer()
+        }
+        .padding(.horizontal)
+        .foregroundColor(.blue)
+    }
+    var remove: some View {
+        Button {
+            if emojiCount > 1  {
+            emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+            }
+        }
+    var add: some View {
+        Button {
+            if emojiCount < emojis.count {
+            emojiCount += 1
+            }
+        } label: {
+            Image(systemName: "plus.circle")
+            }
+        }
+}
+
+struct CardView: View {
+    //Properties...
+    var content: String
+    @State var isFaceUp: Bool = false
+    
+    
     
     var body: some View {
         
@@ -31,20 +69,23 @@ struct CardView: View {
                 shape.fill()
                 shape.foregroundColor(.white)
                 shape.stroke(lineWidth: 5).fill(.orange)
-                Text("〽️")
+                Text(content)
                     .font(.largeTitle)
-            }else{
+            } else {
                 RoundedRectangle(cornerRadius: 20)
                     .fill(.blue)
             }
         }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+            
+        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
             .preferredColorScheme(.dark)
-.previewInterfaceOrientation(.portrait)
+            .previewInterfaceOrientation(.portrait)
     }
 }
