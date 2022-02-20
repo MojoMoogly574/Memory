fae7e282c0233dfda85ef27ae4b68494d071f215
Mojo <mojo@nuralab.com>
2022-02-19 19:38:00 -0600

    Card Game

--- Memory/ContentView.swift
+++ Memory/ContentView.swift
@@ -9,10 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            CardView()
+        }
     }
-}
+        
+    }
+
+
+        struct CardView: View {
+            var body: some View {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 20)
+                        .stroke(lineWidth: 3)
+                    Text("Hello, world!")
+                       
+                }
+            }
+        }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
