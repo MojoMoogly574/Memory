6f9ddab593ab14e97a6feedfc468d887033fe08a
Mojo <mojo@nuralab.com>
2022-02-20 00:27:05 -0600

    adding

--- Memory/ContentView.swift
+++ Memory/ContentView.swift
@@ -11,25 +11,40 @@ struct ContentView: View {
     var body: some View {
         HStack {
             CardView()
-        }
+            CardView()
+            CardView()
+            CardView()
+        }.padding()
+            .foregroundColor(.red)
     }
+    }
+struct CardView: View {
+    
+    var isFaceUp: Bool = false
+    
+    var body: some View {
         
-    }
-
-
-        struct CardView: View {
-            var body: some View {
-                ZStack {
-                    RoundedRectangle(cornerRadius: 20)
-                        .stroke(lineWidth: 3)
-                    Text("Hello, world!")
-                       
-                }
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            
+            if isFaceUp {
+                shape.fill()
+                shape.foregroundColor(.white)
+                shape.stroke(lineWidth: 5).fill(.orange)
+                Text("〽️")
+                    .font(.largeTitle)
+            }else{
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(.blue)
             }
         }
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+.previewInterfaceOrientation(.portrait)
     }
 }
